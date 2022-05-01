bab78b4f11b737baf0eeda1cc6615af31cf3b868
Alejandro R. Mosteo <amosteo@unizar.es>
2022-05-01 12:44:52 +0200

    Experiment for aggregate type error

--- src/yeison_profiles.ads
+++ src/yeison_profiles.ads
@@ -13,6 +13,17 @@ package Yeison_Profiles is
 
    Procedure Append (L : in out List; S : String);
 
+   type Base is tagged null record;
+   type Int is new Base with record
+      I : Integer;
+   end record;
+
+   --  function Make return Base'Class is ((I => 1)); Fails with "Type of
+   --  aggregate cannot be class-wide". This is the same problem afflicting
+   --  our initializations of maps.
+
+   function Make return Base'Class is (Int'(I => 1));
+
 private
 
    type List is tagged null record;
